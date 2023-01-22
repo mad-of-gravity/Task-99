16919283c6248555026b2bf9936a2c263e754709
mad-of-gravity <mad.of.gravity@gmail.com>
2023-01-22 11:47:03 +0200

    Adding style to button

--- src/components/form.js
+++ src/components/form.js
@@ -23,3 +23,16 @@ export const Input = styled.input`
   margin-bottom: 1rem;
   font-size: 0.8rem;
 `;
+
+export default Button = styled.button`
+  background: linear-gradient(to bottom, #6371c7, #5563c1);
+  border-color: #3f4eae;
+  border-radius: 3px;
+  padding: 1rem;
+  color: white;
+  font-weight: 700;
+  width: 100%;
+  margin-bottom: 1rem;
+  font-size: 0.8rem;
+  cursor: pointer;
+`;
