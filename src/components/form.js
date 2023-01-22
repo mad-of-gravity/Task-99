7aea124e9b3058441084e0a738f6f63a79fac7ae
mad-of-gravity <mad.of.gravity@gmail.com>
2023-01-22 11:39:48 +0200

    Add style to the Card component

--- src/components/form.js
+++ src/components/form.js
@@ -1,1 +1,12 @@
 //implement the styled-components logic here.
+import styled from "styled-components";
+
+export const Card = styled.div`
+  box-sizing: border-box;
+  max-width: 410px;
+  margin: 0 auto;
+  padding: 0 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
